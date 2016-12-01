da74cf1cfbbca9e8c6a9d01fbe69393119ebf04d
axel <axelarrondo@gmail.com>
2016-12-01 14:31:56 -0300

    1er commit del hola mundo

---
+++ index.js
@@ -0,0 +1,14 @@
+var http = require("http");
+
+var server = http.createServer();
+
+var time = require('time');
+var now = new time.Date();
+var fechaString = now.toString();
+var soloHora = fechaString.substring(16, 24);
+
+server.on("request",function(req,res){
+	res.end("la hora es: "+soloHora);
+} );
+
+server.listen(3000);
